ce8829ef02e0fbd6e5abe65e1810fa62e8235008
Mark Faine <mfaine@wsl.ndc.nasa.gov>
2025-05-25 13:44:12 -0500

    Make it possible to install with only bootstrap.sh, add colon to when clause in playbook.yml

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,29 +1,28 @@
 #!/usr/bin/env bash
 #shellcheck shell=bash
 set -euo pipefail
-
+set -x
 # Configure a barebones environment for running tooling to configure full development environment
 # Recommend running as a user other than the target user, for example root
 # Running against a host other than localhost will require ssh public key access to the host
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-DEPENDENCIES=(curl wget)
 UV_INSTALL_DIR=/tmp/uv
 UV_INSTALL_SCRIPT="https://astral.sh/uv/install.sh"
 COLLECTION="git+https://github.com/markfaine/net-markfaine.git"
 COLLECTION_BRANCH="experimental"
 
+pushd "$DIR" &>/dev/null || exit 1
+
+
 function install_dependencies() {
-    local tags
     printf "Installing dependencies\n"
-    tags="$(IFS=' '; echo "${dependencies[*]}")"
-    apt install -y "${tag[@]}"
+    apt install -y curl wget
 }
 
 function install_uv(){
     printf "Installing and configuring uv\n"
     curl -LsSf "$UV_INSTALL_SCRIPT" | env UV_INSTALL_DIR="$UV_INSTALL_DIR" sh &>/dev/null
-    mkdir -p $UV_INSTALL_DIR
 }
 
 function install_collection(){
@@ -31,23 +30,33 @@ function install_collection(){
     uvx --from ansible-core ansible-galaxy collection install -f "$COLLECTION,$COLLECTION_BRANCH" &>/dev/null
 }
 
-pushd $UV_INSTALL_DIR &>/dev/null || exit 1
+mkdir -p "$UV_INSTALL_DIR"
+install_dependencies
+install_uv
+
+pushd "$UV_INSTALL_DIR"
+export PATH=".:$PATH"
+install_collection
+
+# If no inventory, use default inventory
+if [[ ! f "$UV_INSTALL_DIR/inventory.yml" ]]; then
+    cp -f "$HOME/.ansible/collections/ansible_collections/net/markfaine/inventory.yml" "$UV_INSTALL_DIR"
+fi
 
 # Set base command
 playbook_cmd=(uvx --with passlib --from ansible-core ansible-playbook)
 
 # Add inventory
-inventory="$1"
-if [[ "${inventory:-}" == "" ]]; then
+inventory="${1:-}"
+if [[ "$inventory" == "" ]]; then
     cp -f "$DIR/inventory.yml" "$UV_INSTALL_DIR/inventory.yml"
     playbook_cmd+=(-i "$UV_INSTALL_DIR/inventory.yml" -l localhost --connection local)
 else
     playbook_cmd+=(-i "$inventory")
 fi
-
 playbook_cmd+=(~/.ansible/collections/ansible_collections/net/markfaine/playbooks/playbook.yml)
 
-printf "Running playbooks\n"
+printf "\nEdit the inventory and then run this command:\n\n"
+
 echo "${playbook_cmd[*]}"
 
-"${playbook_cmd[@]}"
